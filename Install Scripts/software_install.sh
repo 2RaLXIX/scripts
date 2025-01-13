d8e33f4749ae96c06d565016f28e7544354649ca
2RaLXIX <tural.hajibayli19@gmail.com>
2025-01-13 16:15:42 +0400

    update

--- Install Scripts/software_install.sh
+++ Install Scripts/software_install.sh
@@ -24,8 +24,18 @@ echo "All specified packages installed successfully."
 
 # Use sed to uncomment the %wheel line in a safe way
 sudo cp /etc/sudoers /etc/sudoers.bak
+
 #sudo sed -i 's/^#\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD: ALL\)/\1/' /etc/sudoers
-sudo echo "%wheel  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+#sudo echo "%wheel  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+
+# Safely modify the sudoers file
+if sudo grep -q '^#%wheel\s*ALL=(ALL)\s*NOPASSWD:\s*ALL' /etc/sudoers; then
+    # Use sed to uncomment the line
+    sudo sed -i 's/^#\(%wheel\s*ALL=(ALL)\s*NOPASSWD:\s*ALL\)/\1/' /etc/sudoers
+else
+    # Append if the line doesn't exist
+    echo "%wheel  ALL=(ALL)       NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
+fi
 
 read -p "Press any key to reboot"
 sudo reboot now
